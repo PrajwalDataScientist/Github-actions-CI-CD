51677c029f27562c45a08c9b31a0dd1437b5c9bb
PrajwalDataScientist <prajwalint1027@gmail.com>
2025-11-26 14:29:50 +0530

    adding new files

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     responce=app.test_client().get("/home")
     assert responce.status_code==200
-    assert responce.data=="hello prjwal"
+    assert responce.data==b"hello prjwal"
